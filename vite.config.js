ed053a7a62b48f940edb03523943cce2c62ca4b1
tiya <tiyaj0406@gmail.com>
2026-05-23 21:56:05 +0530

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,25 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // <-- Ensure this is present
+  base: "/",
+  build: {
+    // Increase chunk limit to avoid warnings
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        // This stops the builder from crashing by splitting 
+        // the heavy Monaco Editor into its own separate chunk
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("monaco-editor")) {
+              return "monaco";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
